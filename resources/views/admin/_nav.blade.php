b5650052c1af89279cc426e9c2858f46260b368c
Andy <andreii.kachanov@aily.team>
2023-01-07 21:08:31 +0200

    123

--- resources/views/admin/_nav.blade.php
+++ resources/views/admin/_nav.blade.php
@@ -4,7 +4,7 @@
         <li class="nav-item"><a class="nav-link{{ $page === 'categories' ? ' active' : '' }}" href="{{ route('admin.categories.index') }}">Категории</a></li>
         <li class="nav-item"><a class="nav-link{{ $page === 'subcategories' ? ' active' : '' }}" href="{{ route('admin.subcategories.index') }}">Подкатегории</a></li>
         <li class="nav-item"><a class="nav-link{{ $page === 'orders' ? ' active' : '' }}" href="{{ route('admin.orders.index') }}">Заказы</a></li>
-        <li class="nav-item"><a class="nav-link{{ $page === 'settings' ? ' active' : '' }}" href="{{ route('admin.settings.index') }}">Настройки</a></li>
+{{--        <li class="nav-item"><a class="nav-link{{ $page === 'settings' ? ' active' : '' }}" href="{{ route('admin.settings.index') }}">Настройки</a></li>--}}
         <li class="nav-item"><a class="nav-link{{ $page === 'users' ? ' active' : '' }}" href="{{ route('admin.users.index') }}">Users</a></li>
 {{--    @endcan--}}
 </ul>
