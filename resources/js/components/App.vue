3b38999a575714fead7772a827864f373e6d38a0
Andy <andreii.kachanov@aily.team>
2023-01-09 21:51:23 +0200

    123

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -42,6 +42,35 @@
             </div>
         </div>
     </div>
+    <div class="container">
+        <nav class="mobile-bottom-nav">
+            <div class="mobile-bottom-nav__item mobile-bottom-nav__item--active">
+                <div class="mobile-bottom-nav__item-content">
+                    <i class="material-icons">home</i>
+                    one
+                </div>
+            </div>
+            <div class="mobile-bottom-nav__item">
+                <div class="mobile-bottom-nav__item-content">
+                    <i class="material-icons">mail</i>
+                    two
+                </div>
+            </div>
+            <div class="mobile-bottom-nav__item">
+                <div class="mobile-bottom-nav__item-content">
+                    <i class="material-icons">person</i>
+                    three
+                </div>
+            </div>
+
+            <div class="mobile-bottom-nav__item">
+                <div class="mobile-bottom-nav__item-content">
+                    <i class="material-icons">phone</i>
+                    four
+                </div>
+            </div>
+        </nav>
+    </div>
 </template>
 
 <script>
@@ -73,4 +102,41 @@ $fa-font-path : "~@fortawesome/fontawesome-free-webfonts/webfonts";
 @import "~@fortawesome/fontawesome-free-webfonts/scss/fa-solid.scss";
 @import "~@fortawesome/fontawesome-free-webfonts/scss/fa-regular.scss";
 @import "~@fortawesome/fontawesome-free-webfonts/scss/fa-brands.scss";
+
+.mobile-bottom-nav{
+    position:fixed;
+    bottom:0;
+    left:0;
+    right:0;
+    z-index:1000;
+
+    //give nav it's own compsite layer
+    will-change:transform;
+    transform: translateZ(0);
+
+    display:flex;
+
+    height:50px;
+
+    box-shadow: 0 -2px 5px -2px #333;
+    background-color:#fff;
+
+    &__item{
+        flex-grow:1;
+        text-align:center;
+        font-size:12px;
+
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+    }
+    &__item--active{
+        //dev
+        color:red;
+    }
+    &__item-content{
+        display:flex;
+        flex-direction:column;
+    }
+}
 </style>
