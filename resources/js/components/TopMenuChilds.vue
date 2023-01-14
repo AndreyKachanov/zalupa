f33bce921cf927f192a54f1bafe6024ff7c46648
Andy <andreii.kachanov@aily.team>
2023-01-14 19:51:53 +0200

    123

--- resources/js/components/TopMenuChilds.vue
+++ resources/js/components/TopMenuChilds.vue
@@ -3,12 +3,10 @@
     <div class="container-fluid top-menu">
         <div class="row">
             <div class="col-8 offset-2 parent">
-                <p>
                     {{ parentTitle }}
-                </p>
             </div>
                 <router-link
-                    class="child col-4 offset-2 btn btn-success"
+                    class="child col-4 offset-2"
                     v-for="category in categories"
                     :key="category.route"
                     :to="`/category/${category.slug}`"> {{ category.title }}
@@ -29,10 +27,38 @@ export default {
 <style lang="scss">
     .top-menu {
         .parent {
-            border: 1px solid red;
             text-align: center;
+            display: block;
+            color: white;
+            font-weight: 700;
+            text-decoration: none;
+            user-select: none;
+            padding: 1em 2em;
+            border: 1px solid;
+            border-radius: 1px;
+            word-break: break-all;
+            background-color: #c1034a;
+            //&:hover {
+            //    background: #c4205c;
+            //}
         }
         .child {
+            text-align: center;
+            display: block;
+            color: white;
+            background-color: #454545;
+            font-weight: 700;
+            text-decoration: none;
+            user-select: none;
+            padding: 1em 2em;
+            outline: none;
+            border: 1px solid;
+            border-radius: 1px;
+            transition: 0.2s;
+            word-break: break-all;
+            &:hover {
+                background: #454545bf;
+            }
             &:nth-child(odd) {
                 margin-left: 0 !important;
             }
