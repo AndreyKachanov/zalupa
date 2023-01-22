a9255e5bd96191ee17c12bdc78dcce9a66470bf7
Andy <andreii.kachanov@aily.team>
2023-01-22 12:32:14 +0200

    123

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,7 +111,7 @@
 
         @yield('scripts')
 
-        @env('production')
+        @env(['production', 'dev'])
             @if($pageWithVue)
                 <script type="text/javascript" >
                     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
